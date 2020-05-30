1520b2faeae2a33a9f09afd46d365b477468a873
geetham04 <56003084+geetham04@users.noreply.github.com>
2020-05-30 15:28:20 -0700

    Update query1.py

--- src/query1.py
+++ src/query1.py
@@ -6,8 +6,8 @@ client = MongoClient('mongodb://localhost:27017/')
 
 with client:
 
-    db = client.projectpart2
+    db = client.Freeway
 
-    number_speed = db.loopdataAll.count({'speed': {'$gt':100}})
+    number_speed = db.loopdata.count_documents({'speed': {'$gt':100}})
 
     print("There are {} number of speeds".format(number_speed))
